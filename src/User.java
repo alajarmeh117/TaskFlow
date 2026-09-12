9a3c57c620236f99b77f17b28371cecf522a6eee
jourycosmetics73 <jourycosmetics73@gmail.com>
2026-09-12 18:29:02 +0300

    Update User Class

--- src/User.java
+++ src/User.java
@@ -3,7 +3,19 @@ public class User {
 	
 	  private int id ; 
 	  private String Name ; 
-	  private String Email ; 
+	  private String Email ;
+	  private String numberPhone ;
+	  
+	  
+	  public User(int id, String name, String email, String numberPhone) {
+		super();
+		this.id = id;
+		Name = name;
+		Email = email;
+		this.numberPhone = numberPhone;
+	  } 
+	  
+	  
 	  
 	
 }
