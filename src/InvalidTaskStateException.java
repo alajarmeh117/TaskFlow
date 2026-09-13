529b40b13d2d9609592b8bda3bd782b91e9dfb93
jourycosmetics73 <jourycosmetics73@gmail.com>
2026-09-13 19:55:20 +0300

    Add Task constructors, state management methods, and custom exception For InvalidTaskState

---
+++ src/InvalidTaskStateException.java
@@ -0,0 +1,12 @@
+
+public class InvalidTaskStateException extends Exception{
+	
+	public InvalidTaskStateException(String message) {
+		
+	  //  System.out.println(e.getMessage()); --> [ return message from Execption ]  
+
+		super(message);
+		
+	}
+
+}
--- src/Task.java
+++ src/Task.java
@@ -18,5 +18,34 @@ public class Task {
 	}
 	
 	
+       //  default constructor 
+ 	public Task() {       
+		this.status = TaskStatus.TODO;
+	}
+
+	public Task(User owner, String title, LocalDateTime dueDate, Task.PriorityLevel priority) {
+		super();
+		this.owner = owner;
+		this.title = title;
+		this.dueDate = dueDate;
+		this.priority = priority;
+	}
+	
+	public void  start() {    //  To change the status from TODO to IN_PROGRESS
+		
+		
+	}
+	 
+	public void complete() {  // To change the status from IN_PROGRESS  to DONE
+		
+		
+	}
+	
+	public void markOverdue() { // The mission time ended before it was completed ( DONE ) .
+
+		
+	}
+	
 
 }
+
